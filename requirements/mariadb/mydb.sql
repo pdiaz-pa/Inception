31fcc491004f6e239e3b9486de28e82b0ccaa383
Pablo Diaz pastor <pdiaz-pa@c2r7s5.42madrid.com>
2022-04-27 18:01:09 +0200

    tal

---
+++ requirements/mariadb/mydb.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE IF NOT EXISTS wp_db;
+CREATE USER 'pdiaz-pa'@'localhost';
+SET PASSWORD FOR 'pdiaz-pa'@'localhost' = PASSWORD('caldodepollo');
+GRANT ALL PRIVILEGES ON wp_db.* TO 'pdiaz-pa'@'localhost' IDENTIFIED BY 'caldodepollo';
+GRANT ALL ON wp_db.* to 'pdiaz-pa'@'%' IDENTIFIED BY 'caldodepollo';
+
